1b3be9d160b8913cdbdf65a19dfa334e710d20d5
sunnus01 <haapsalu.sun@gmail.com>
2019-06-13 14:05:13 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,15 +1,30 @@
 <?php
+$stid = 76561198797555452;
 require 'includes/configg123.php';
-$captenid = array();
+$captains = array();
 $conn = new mysqli($servername, $username, $password, $dbmix);
 	$sql1 = "SELECT steamid, timetopick FROM tournament WHERE capt = '1'";
 	$result = $conn->query($sql1);
 		if ($result->num_rows > 0) {
 			while($row = $result->fetch_assoc()) {
-				array_push($captenid[$row["steamid"]] = $row["timetopick"]);
+				array_push($captains[$row["steamid"]] = $row["timetopick"]);
 			}
 		}
 
 
-print_r ($captenid); // Returns the key and value of the current element (now Joe), and moves the internal pointer forward
+print_r ($captenid); // Returns the key and value of the current element, and moves the internal pointer forward
+
+echo "Captains:";
+$kogus = count($captains);
+foreach ($captains as $key => $val) {
+	if ($key == $stid && $val == 1) {
+	echo "<br>Your time to pick</br>";
+	break;
+	}
+	else {
+		echo "Another captain is picking";
+		break;
+	}
+}
 ?>
+
